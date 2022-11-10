7656018edc651d979ae2677b1ae3d8832d49ee20
owen28445 <Fengji@ssfs.org>
2022-11-10 11:15:10 -0500

    finsihed

--- for_loops.py
+++ for_loops.py
@@ -1,19 +1,19 @@
 #Owen 10/11/2022
 def count(first, last):
-    """
-    This function will create a string of numbers separated by a space. The numbers will start with the
-    first number and end with the second. The second number SHOULD be included as part of the string. If
-    the first number is larger than the second, the numbers should count down, rather than up.
-    count(5, 10) returns "5 6 7 8 9 10 "
-    :param first: The starting number
-    :param second: The final number. Must be included
-    :return: A string containing the numbers
-    """
-    pass # make sure to delete this line when you write your own function
+    test = ""
+    for x in range(first, last):
+        test += str(x)
+    print(test)
+
 
 
 def main():
-    print(count(0, 6))
+    (count(0, 7)
+    (count(7, 0)
+
+
+
+
 
 
 if __name__ == '__main__':
