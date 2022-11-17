5eda4f7ed297aa5c5ea554fa98e5ab99c97015f1
owen28445 <Fengji@ssfs.org>
2022-11-17 11:57:12 -0500

    finsihed

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,11 @@
-def fibonacci(x):
-    """
-    Ex. fibonacci(5) returns "1 1 2 3 5 "
-    :param number: The number of Fibonacci terms to return
-    :return: A string consisting of a number of terms of the Fibonacci sequence.
-    """
+def fib(number):
+    a = 1
+    b = 1
+    List_of_numbers  = ""
+    for x in range(7):
+        c = a +b
+        List_of_numbers += str(c) + ""
+        b = a
+        print(List_of_numbers)
+
+fib (4)
--- multiples.py
+++ multiples.py
@@ -1,10 +1,10 @@
 def get_sum(number):
-    """
-    Will return the sum of all the multiples of 3 or 5 of the given number.
-    Ex. get_sum(10) returns 23
-    :param number: The value up to which we want to find the sum. Not included in the calculation
-    :return: The sum of all the multiples of 3 or 5 up to the number.
-    """
+    number = 4
+    if number 2 = =0
+    print(The number is even")
+    else:
+    print("The number is odd")
+  total += (6)
     pass # make sure to delete this line when writing your function
 
 
