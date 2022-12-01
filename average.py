fe73e2286ae7969d9ef4ad11ee2626253631399d
owen28445 <Fengji@ssfs.org>
2022-12-01 11:11:53 -0500

    finsihed thanks

---
+++ average.py
@@ -0,0 +1,13 @@
+def Calculating (averages)
+print(13 -> 13*3 + 1 = 40)
+print(40 ->  40/2 = 20)
+print(20 -> 20 / 2 = 10)
+print(10 -> 10/2 = 5)
+print(5 -> 3*5 + 1 = )16
+print(16 -> 16/2 = 8)
+print(8 -> 8/2 = 4)
+print(2 -> 2/2 = 1)
+
+
+total = Calculating averages:
+
