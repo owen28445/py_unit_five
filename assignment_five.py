ce30619931c782fd8a0bcac465124e7ae5305586
owen28445 <Fengji@ssfs.org>
2022-12-12 14:21:14 -0500

    finsihed thanks for you help

---
+++ assignment_five.py
@@ -0,0 +1,19 @@
+def random():
+
+    number = random.randint(1, 100)
+    print('Well, '', I am thinking of a number between 1 and 100.')
+    return number
+
+def getguess
+    print('Take a guess.')
+    guess = input()
+    return guess
+
+
+def main
+    
+
+
+
+
+
