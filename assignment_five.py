4efb6b8c93f055ef589785fc512761b24d522b01
owen28445 <Fengji@ssfs.org>
2022-12-14 13:08:13 -0500

    finsihed thanks for you help

--- assignment_five.py
+++ assignment_five.py
@@ -1,16 +1,34 @@
-def random():
-
+import random
+'''this program is about guess a number, the user need to enter the number and take a guess'''
+def get_random():
     number = random.randint(1, 100)
     print('Well, '', I am thinking of a number between 1 and 100.')
     return number
 
-def getguess
+def getguess():
     print('Take a guess.')
     guess = input()
     return guess
 
 
-def main
+def main():
+    randomnumbers = int(get_random())
+
+    while True:
+
+        getguessnumbers = int(getguess())
+        if randomnumbers < getguessnumbers:
+            print("your number is too high")
+
+        if randomnumbers > getguessnumbers:
+            print("your number is too low")
+
+        if randomnumbers == getguessnumbers:
+            print("the number is correct")
+            break
+
+
+main()
     
 
 
